6e0da0b94391eebb0476bce6c40a94d2e8677154
pranav <pranavdeval@gmail.com>
2019-08-10 23:25:36 +0530

    add mongoDB atlas

--- app.js
+++ app.js
@@ -17,7 +17,16 @@ var commentRoutes    = require("./routes/comments");
 var indexRoutes       = require("./routes/index");
 
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
+// mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://pranavdeval:Adilabad1@cluster0-onhsi.mongodb.net/test?retryWrites=true&w=majority",{
+    useNewUrlParser: true,
+    useCreateIndex: true
+}).then( function() {
+    console.log("Connected!");
+}).catch(err => {
+    console.log("ERROR:",err.message);
+});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"))    //  __dirname indiacates the current directory
